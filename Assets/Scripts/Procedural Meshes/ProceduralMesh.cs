ae45f45ce8b02cdf58312a49344c33f5ae07c93d
PhilipRobertsson <phiro138@student.liu.se>
2024-12-30 15:16:17 +0100

    Added gizmos to procedural mesh
    Gizmos used to visualize vertex positions, normals and, tangents.

--- Assets/Scripts/Procedural Meshes/ProceduralMesh.cs
+++ Assets/Scripts/Procedural Meshes/ProceduralMesh.cs
@@ -33,10 +33,58 @@ public class ProceduralMesh : MonoBehaviour{
     }
     void OnValidate() => enabled = true;
 
+    [SerializeField]
+    GizmoMode gizmos;
+
+    [System.Flags]
+    public enum GizmoMode { Nothing = 0, Vertices = 1, Normals = 0b10, Tangents = 0b100 }
+
+    Vector3[] vertices, normals;
+    Vector4[] tangents;
     void Update()
     {
         GenerateMesh();
         enabled = false;
+
+        vertices = null;
+        normals = null;
+        tangents = null;
+    }
+
+    void OnDrawGizmos(){
+        if (gizmos == GizmoMode.Nothing || mesh == null){
+            return;
+        }
+        bool drawVertices = (gizmos & GizmoMode.Vertices) != 0;
+        bool drawNormals = (gizmos & GizmoMode.Normals) != 0;
+        bool drawTangents = (gizmos & GizmoMode.Tangents) != 0;
+
+        if (vertices == null){
+            vertices = mesh.vertices;
+        }
+        if (drawNormals && normals == null){
+            normals = mesh.normals;
+        }
+        if (drawTangents && tangents == null){
+            tangents = mesh.tangents;
+        }
+
+        Transform t = transform;
+        for (int i = 0; i < vertices.Length; i++){
+            Vector3 position = t.TransformPoint(vertices[i]);
+            if (drawVertices){
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(position, 0.02f);
+            }
+            if (drawNormals){
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(position, t.TransformDirection(normals[i]) * 0.2f);
+            }
+            if (drawTangents){
+                Gizmos.color = Color.red;
+                Gizmos.DrawRay(position, t.TransformDirection(tangents[i]) * 0.2f);
+            }
+        }
     }
 
     [SerializeField, Range(1, 50)]
